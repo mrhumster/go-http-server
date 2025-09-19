c71b0d0db0197b9da27c39b5194a76c811413b09
Om Ra <me@xomrkob.ru>
2025-09-19 12:22:09 +0600

    add testing to ci

--- main_test.go
+++ main_test.go
@@ -1,24 +1,35 @@
 package main
 
 import (
-	"io/ioutil"
+	"context"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetHello(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/hello", nil)
+	responseBody := "Hello, Billy!"
+	payload := strings.NewReader("myName=Billy")
+
+	req := httptest.NewRequest(http.MethodPost, "/hello", payload)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	ctx := context.WithValue(req.Context(), "serverAddr", "httptest")
+	req = req.WithContext(ctx)
 	w := httptest.NewRecorder()
+	// Run Handler
 	getHello(w, req)
+
 	res := w.Result()
 	defer res.Body.Close()
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Errorf("expected error to be nil got %v", err)
-	}
-	if string(data) != "ABC" {
-		t.Errorf("expected ABC")
-	}
+
+	assert.NotEqual(t, http.StatusOK, res.StatusCode)
+	data, err := io.ReadAll(res.Body)
+	require.NoError(t, err)
+	assert.Equal(t, responseBody, strings.TrimSpace(string(data)))
 
 }
